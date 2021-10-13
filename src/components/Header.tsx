ce86dd7acc509dca84427e2ad79b7eee733624ed
Cleyton Santana <csttn.dev@gmail.com>
2021-10-13 15:57:45 -0300

    create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Box, Container } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/image';
+import React from 'react';
+
+import Logo from '../assets/Logo.svg';
+
+const Header: React.FC = () => {
+  return (
+    <Box w='100%' p='4' display='flex' alignContent='center' justifyContent='center'>
+      <Image src={Logo} h='20 ' />
+    </Box>
+  );
+};
+
+export {Header}
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Header } from '../../components/Header';
+
+const HomePage: React.FC = () => {
+  return <Header />;
+};
+
+export { HomePage };
